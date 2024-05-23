c770d516c1e34282ee6a5956857fe9c7434dd0a9
lotsoo <braindeathrama@gmail.com>
2024-05-23 18:46:44 +0700

    Added image

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
@@ -32,3 +36,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun TaskManagerApp(image: Painter, modifier: Modifier = Modifier) {
+    Column {
+        Image(painter = image, contentDescription = null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TaskManagerAppPreview() {
+    TaskManagerApp(
+        image = painterResource(R.drawable.ic_task_completed),
+    )
+}
