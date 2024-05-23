eace6b53dfe2e65edd405de06426d33586b9c6bd
lotsoo <braindeathrama@gmail.com>
2024-05-23 20:42:34 +0700

    Added Text

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -5,18 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    TaskManagerApp(image = painterResource(id = R.drawable.ic_task_completed))
                 }
             }
         }
@@ -37,9 +43,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TaskManagerApp(image: Painter, modifier: Modifier = Modifier) {
-    Column {
+fun TaskManagerApp(image: Painter,  modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Image(painter = image, contentDescription = null)
+        Text(
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
